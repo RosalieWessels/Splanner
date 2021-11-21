82fa3214760473e176f540dc7b1c92cd6c4f2ef7
RosalieWessels <rosalie.wessels@yahoo.com>
2021-11-21 12:27:43 -0800

    Revert "Merge remote-tracking branch 'refs/remotes/origin/main'"
    This reverts commit f1a6131b2652726069ea0d8641ddfd266045dcd5, reversing
    changes made to a37f654a735080d4e6703aee9ce375e574fab92b.

--- Shared/SplannerApp.swift
+++ Shared/SplannerApp.swift
@@ -7,112 +7,17 @@
 
 import SwiftUI
 import Firebase
-import FirebaseMessaging
 
 @main
 struct SplannerApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-//    init() {
-//        FirebaseApp.configure()
-//    }
+    init() {
+            FirebaseApp.configure()
+        }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    let gcmMessageIDKey = "gcm.message_id"
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        
-        FirebaseApp.configure()
-        
-        //Setting up...
-        
-        Messaging.messaging().delegate = self
-        
-        if #available(iOS 10.0, *) {
-          // For iOS 10 display notification (sent via APNS)
-          UNUserNotificationCenter.current().delegate = self
-
-          let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-          UNUserNotificationCenter.current().requestAuthorization(
-            options: authOptions,
-            completionHandler: { _, _ in }
-          )
-        } else {
-          let settings: UIUserNotificationSettings =
-            UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-          application.registerUserNotificationSettings(settings)
-        }
-
-        application.registerForRemoteNotifications()
-        
-        
-        return true
-    }
-    
-    func application(_ application: UIApplication,
-                     didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult)
-                       -> Void) {
-      //do something with message data here
-      if let messageID = userInfo[gcmMessageIDKey] {
-        print("Message ID: \(messageID)")
-      }
-
-      // Print full message.
-      print(userInfo)
-
-      completionHandler(UIBackgroundFetchResult.newData)
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        
-    }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
-    }
-}
-
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      print(dataDict)
-    }
-
-}
-
-@available(iOS 10, *)
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        let userInfo = notification.request.content.userInfo
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-        
-        completionHandler([[.banner, .badge, .sound]])
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-
-        completionHandler()
     }
 }
