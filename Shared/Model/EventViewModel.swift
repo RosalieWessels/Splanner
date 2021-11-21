3544031e999b0d8a2220ddf0bcda07027a3aa2a3
RosalieWessels <rosalie.wessels@yahoo.com>
2021-11-21 12:09:11 -0800

    started student schedule screen

---
+++ Shared/Model/EventViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  EventViewModel.swift
+//  Splanner
+//
+//  Created by Rosalie Wessels on 11/21/21.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class eventViewModel: ObservableObject {
+    
+    @Published var events = [Event]()
+    
+    private var db = Firestore.firestore()
+    
+    func fetchData() {
+        db.collection("event").order(by: "date", descending: false).addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            self.events = documents.map { (queryDocumentSnapshot) -> Event in
+                let data = queryDocumentSnapshot.data()
+                let title = data["title"] as? String ?? ""
+                let dateEvent = (data["date"] as? Timestamp)?.dateValue() ?? Date()
+                return Event(title: title, dateEvent: dateEvent)
+            }
+        }
+    }
+}
--- Shared/SplannerApp.swift
+++ Shared/SplannerApp.swift
@@ -19,7 +19,7 @@ struct SplannerApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            StudentHomeView()
         }
     }
 }
