8d0b4a506c2803921f7ca3b37be5f238ecd1b00d
RosalieWessels <rosalie.wessels@yahoo.com>
2021-11-21 07:26:53 -0800

    Initial Commit

---
+++ Shared/SplannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplannerApp.swift
+//  Shared
+//
+//  Created by Rosalie Wessels on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct SplannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
